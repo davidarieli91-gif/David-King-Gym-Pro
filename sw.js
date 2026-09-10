39cb51ad7e36748428705f505e59101b19ab8ae1
Z User <z@container>
2026-09-10 17:56:01 +0000

    GI instructions TM HE 91-92 (24340/8045 left), seed v73 (c39)

--- sw.js
+++ sw.js
@@ -18,7 +18,8 @@
 // v62: GI translations RU 118-119 + HE 83-84, seed v69 (c35)
 // v63: GI translations RU 120-121 RU COMPLETE 100% + HE 85-86, seed v70 (c36)
 // v64: GI translations HE 87-88, seed v71 (c37)
-const CACHE_NAME = 'dk-gym-v65'; // v65: GI translations HE 89-90, seed v72 (c38)
+// v65: GI translations HE 89-90, seed v72 (c38)
+const CACHE_NAME = 'dk-gym-v66'; // v66: GI translations HE 91-92, seed v73 (c39)
 const APP_SHELL = [
   './',
   './index.html',
